84c0852d923a4de3bb780c1f7281fdc0c29992dc
rudolf <rudolf@example.com>
2020-09-08 17:47:33 +0100

    first commit and included mapbox with config

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useEffect } from "react";
+import mapboxgl from "mapbox-gl";
+import "./App.scss";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
+
+const App = () => {
+    const mapContainerRef = useRef(null);
+
+    // initialize map on mount
+    useEffect(() => {
+        const map = new mapboxgl.Map({
+            container: mapContainerRef.current,
+            style: 'mapbox://styles/mapbox/streets-v11',
+            center: [-104.9876, 39.7405],
+            zoom: 12.5,
+        });
+
+        // zoom btn control
+        map.addControl(new mapboxgl.NavigationControl(), 'bottom-right');
+
+        // clean up on unmount
+        return () => map.remove();
+    }, []);
+
+    return <div className="map-container" ref={mapContainerRef} />;
+};
 
 export default App;
